2cc864de03eea4562af1a8c3d239715c6b99c800
javad yousefi <javad.yousefi373@gmail.com>
2025-12-27 18:15:57 +0330

    bug fixes1

--- src/app/admin/sliders/create/page.tsx
+++ src/app/admin/sliders/create/page.tsx
@@ -1,1 +1,14 @@
- 
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function CreateSliderPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/admin/sliders')
+  }, [router])
+
+  return null
+}
--- src/app/admin/sliders/edit/[id]/page.tsx
+++ src/app/admin/sliders/edit/[id]/page.tsx
@@ -1,1 +1,14 @@
- 
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function EditSliderPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/admin/sliders')
+  }, [router])
+
+  return null
+}
